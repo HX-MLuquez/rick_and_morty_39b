144bc6b2a83c5db3c4c9e07d28989f73d4b1486d
Mauricio776 <mauuuricio@hotmail.com>
2023-06-12 14:03:37 -0300

    init PI rick and Morty

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+export default function Card({char, onClose}) { // obj
+   const {id,name,gender,species,origin,image,status} = char
+   return (
+      <div>
+         <button onClick={onClose}>X</button>
+         <h2>{name}</h2>
+         <h2>{status}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin?.name}</h2>
+         <img src={image} alt={name} />
+      </div>
+   );
+}
+// namePepe: nombre.
+// status: status.
+// species: especie.
+// gender: género.
+// origin: origen (ten en cuenta que el nombre del origen viene dentro de otra propiedad llamada name).
+// image: imagen.
